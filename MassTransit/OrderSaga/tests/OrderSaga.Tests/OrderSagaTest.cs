cdc3fc1830dba2e1526a2a14e9b5d57511b4de34
Evrim <evrim@tr.ibm.com>
2020-10-09 14:34:22 +0300

    Added unit tests

---
+++ MassTransit/OrderSaga/tests/OrderSaga.Tests/OrderSagaTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using OrderCommon.Contracts;
+using OrderCommon.Models;
+using OrderSaga.StateMachine;
+
+namespace OrderSaga.Tests
+{
+    public class OrderSagaTest
+    {
+        [Test]
+        public async Task Should_test_order_saga()
+        {
+
+            var collection = new ServiceCollection();
+            collection.AddLogging(c => c.AddConsole());
+            collection.RegisterSagaStateMachine<OrderStateMachine, OrderState>();
+            collection.AddScoped<SendAcceptOrderCommandActivity>();
+            collection.AddMassTransitInMemoryTestHarness(x =>
+            {
+                x.AddSagaStateMachine<OrderStateMachine, OrderState>()
+                    .InMemoryRepository();
+
+                x.AddSagaStateMachineTestHarness<OrderStateMachine, OrderState>();
+            });
+
+            var serviceProvider = collection.BuildServiceProvider();
+
+            var harness = serviceProvider.GetRequiredService<InMemoryTestHarness>();
+            var machine = serviceProvider.GetRequiredService<OrderStateMachine>();
+
+            harness.OnConfigureInMemoryReceiveEndpoint += cfg =>
+                cfg.StateMachineSaga(machine, serviceProvider);
+
+            await harness.Start();
+
+            var sagaHarness = serviceProvider.GetRequiredService<IStateMachineSagaTestHarness<OrderState, OrderStateMachine>>();
+
+            try
+            {
+                Guid sagaId = NewId.NextGuid();
+                Console.WriteLine($"sagaId: {sagaId}");
+
+                //var sendEndpoint = await harness.Bus.GetSendEndpoint(new Uri("queue:order-saga"));
+                //await sendEndpoint.Send<ISubmitOrder>(new {Customer="1"});
+
+                await harness.Bus.Publish<ISubmitOrder>(new { OrderId = sagaId, Customer = "1" });
+                //await harness.BusSendEndpoint.Send<ISubmitOrder>(new { OrderId = sagaId, Customer = "1" });
+
+                // did the endpoint consume the message
+                Assert.IsTrue(harness.Consumed.Select<ISubmitOrder>().Any());
+
+                // did the actual consumer consume the message
+                Assert.IsTrue(sagaHarness.Consumed.Select<ISubmitOrder>().Any());
+
+                var instance = sagaHarness.Created.ContainsInState(sagaId, machine, machine.Submitted);
+                Assert.IsNotNull(instance, "Saga instance not found");
+
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+
+    }
+}
---
+++ MassTransit/OrderSaga/tests/OrderSaga.Tests/Using_the_test_harness.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit.Testing;
+using NUnit.Framework;
+
+namespace OrderSaga.Tests
+{
+    public class Using_the_test_harness
+    {
+
+        [Test]
+        public async Task Should_run_InMemoryTestHarness()
+        {
+            var harness = new InMemoryTestHarness();
+
+            await harness.Start();
+
+            try
+            {
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+        }
+    }
+}
